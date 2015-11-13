2bae8401d9aef5c8708ca34e1251a66150d5bda8
adamsar <adamsar@gmail.com>
2015-11-13 14:25:18 +0900

    Add in rotating colors

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,27 @@ require('styles/App.css');
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
+const colors = ['red', 'white', 'green'];
+let currentIndex = 0;
+function nextColor() {
+  let color = colors[currentIndex % colors.length];
+  currentIndex++;
+  return color;
+}
 
 class AppComponent extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.state = { background: 'red' };
+    this.state = { background: nextColor() };
   }
+
+  componentDidMount() {
+    let self = this;
+    setInterval(function () {
+      self.setState({background: nextColor()});
+      }, 3000);
+  }
+
   render() {
     return (
       <div className="index">
